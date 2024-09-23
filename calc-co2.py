0d1693ad17203024670ff95327fda114076ed773
Jaka Kranjc <smgl@lynxlynx.info>
2024-09-23 22:46:21 +0200

    the site changed ... in some ways for the better, in some for the worse

--- calc-co2.py
+++ calc-co2.py
@@ -2,10 +2,12 @@
 import csv
 import sys
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.select import Select
 
 # DEPENDENCY INFO:
 # install geckodriver or chromedriver
@@ -85,64 +87,66 @@ def parseEntry(row, writer):
 def runTest(start, end, mode, fuel):
     browser.get("https://travelandclimate.org/")
     assert 'Travel' in browser.title
+
     # 1 person
-    browser.find_element(By.ID, "edit-field-antal-personer-0-value").click()
-    browser.find_element(By.ID, "edit-field-antal-personer-0-value").send_keys("1")
+    browser.find_element(By.ID, "people").click()
+    browser.find_element(By.ID, "people").send_keys("1")
 
     # set one-way trip
-    # maybe not needed
-    browser.find_element(By.ID, "edit-field-antal-personer-wrapper").click()
-    browser.find_element(By.LINK_TEXT, "return").click()
-    browser.find_element(By.CSS_SELECTOR, ".nl-field li:nth-child(1)").click()
+    browser.find_element(By.ID, "ways").click()
+    browser.find_element(By.CSS_SELECTOR, "#ways > :nth-child(2)").click()
 
     # set start
-    browser.find_element(By.ID, "edit-field-fran-0-value").click()
-    browser.find_element(By.ID, "edit-field-fran-0-value").send_keys(start)
+    browser.find_element(By.ID, "origin").click()
+    browser.find_element(By.ID, "origin").send_keys(start)
     fakeWait(1)
     waitForVisible(10, (By.CSS_SELECTOR, ".pac-item:nth-child(1)"))
     browser.find_element(By.CSS_SELECTOR, ".pac-item:nth-child(1)").click()
 
     # set destination
-    browser.find_element(By.ID, "edit-field-till-0-value").click()
-    browser.find_element(By.ID, "edit-field-till-0-value").send_keys(end)
+    browser.find_element(By.ID, "destinations.0").click()
+    browser.find_element(By.ID, "destinations.0").send_keys(end)
     fakeWait(1)
     waitForVisible(10, (By.CSS_SELECTOR, ".pac-item:nth-child(1)"))
-    browser.find_element(By.CSS_SELECTOR, ".pac-item:nth-child(1)").click()
+    destEl = browser.find_element(By.CSS_SELECTOR, ".pac-item:nth-child(1)")
+    # sigh, child blocks it, but luckily is not a full overlay
+    elH = destEl.rect["height"]
+    elW = destEl.rect["width"]
+    ActionChains(browser).move_to_element_with_offset(destEl, - elW // 4, elH // 2 - 1).click().perform()
 
     # ignore sleep and trigger calc
-    browser.find_element(By.ID, "edit-field-antal-natter-0-value").click()
-    browser.find_element(By.ID, "edit-field-antal-natter-0-value").send_keys("0")
-    browser.find_element(By.ID, "edit-field-antal-natter-0-value").send_keys(Keys.ENTER)
+    browser.find_element(By.XPATH, "//button[contains(text(), 'Calculate')]").click()
 
     # wait for initial calc and then choose transport type
     # to id xpath in dev tools: $x("some xpath")
     # normal car:
-    button = "//span[contains(@class, 'column-sub-title')]/div[text()='Car']"
-    waitForVisible(30, (By.XPATH, button))
-    # NOTE: button order can be inconsistent, so we target by text
+    button = "t-driving"
     scaleFactor = 1
     originalMode = mode
-    if mode == "Car" and fuel == "Electricity":
-        # NOTE: uses nordic electricity numbers if appropriate
-        button = "//span[contains(@class, 'column-sub-title')]/div[text()='El.car']"
-    elif mode == "Car":
+    colSelector = ".column-driving .bg-drivingcar, .column-driving .bg-carferrycar"
+    if mode == "Car":
         pass
     elif mode == "Bus" or mode == "Train":
         # NOTE: sometimes only offers a train for bus rides,
         # probably vice-versa as well. However the emission factors are
         # similar, so we don't mind
-        button = "//span[contains(@class, 'column-sub-title')]/div[text()='Train / bus']"
+        button = "t-public-transport"
+        colSelector = ".column-public-transport .bg-publictransport"
     elif mode == "Plane":
         # NOTE: includes shuttle to city if relevant
-        button = "//span[contains(@class, 'column-sub-title')]/div[text()='Air']"
+        # FIXME: long flights can have so high emissions the chart offsets elements enough
+        # for this to fail, eg. with Houston to Athens
+        button = "t-flying"
+        colSelector = ".column-flying > .bg-flying"
     elif mode == "Ferry":
         # taken into account internally under Car
         # FIXME: unless there's no other option, then there's an independent ferry button
         # hit pe. with Helsinki - Tallinn
         return 0, 0
     elif mode == "Motorbike":
-        # NOTE: compare average fuel consumption is tricky, after some scouring
+        # NOTE: comparing average fuel consumption is tricky, after some scouring
         # we take half of the car value
+        mode = "Car"
         scaleFactor = 0.5
     elif mode == "Bike" or mode == "Walk":
         mode = "Car"
@@ -151,71 +155,61 @@ def runTest(start, end, mode, fuel):
         return errorValue, errorValue
 
     # actually pick ride type
-    browser.find_element(By.XPATH, button).click()
-    otherFuels = [ "Petrol", "Natural gas", "Mix of natural and biogas", "Biogas", "Ethanol", "Biodiesel" ]
+    if mode == "Plane":
+        pass
+        #import pdb; pdb.set_trace()
+    waitForVisible(10, (By.CLASS_NAME, button))
+    ride = browser.find_element(By.CLASS_NAME, button)
+    if ride:
+        ride.click()
+    else:
+        print("No known connection for leg type: " + mode)
+        return errorValue, errorValue
 
     # extra steps to get length of leg and pick fuel
-    # NOTE: there could be more than one to pick (eg. with interim ferries or for airport access)
+    # NOTE: there could be more than one to pick (eg. with interim ferries, for airport access or changing buses)
 
-    # click again to trigger the overlay, but the button path changed ...
-    browser.find_element(By.XPATH, button).click()
-
-    # sigh, of course the correct way to do it doesn't work
-    #waitForVisible(20, (By.CLASS_NAME, "to-km"))
+    # open details column overlay and extract leg distance
+    # there can be more than one valid bar (common with buses or if a ferry interrupts cars)
     fakeWait(1)
-    els = browser.find_elements(By.CLASS_NAME, "to-km")
-    km = sum(map(lambda el: int(el.text.split()[0]) if el.is_displayed() else 0, els))
+    waitForVisible(5, (By.CSS_SELECTOR, colSelector))
+    #import pdb; pdb.set_trace()
+    bars = browser.find_elements(By.CSS_SELECTOR, colSelector)
+    km = 0
+    seenKMs = [] # hack to avoid occasional double counting
+    for bar in bars:
+        if not bar.is_displayed() or "hidden" in bar.get_dom_attribute("class"):
+            continue
+        ActionChains(browser).move_to_element(bar).perform()
+        fakeWait(1)
+        # yuck, occasional XPATH failure, so we have to filter manually
+        kmEls = bar.find_elements(By.XPATH, "//div/div/div[contains(text(), ' km')]")
+        for kmEl in kmEls:
+            if " km" in kmEl.text and kmEl.text not in seenKMs:
+                seenKMs.append(kmEl.text)
+                km += int(kmEl.text.split()[0])
+                break
 
+    # change fuel if not diesel
+    otherFuels = [ "Petrol", "Natural gas", "Biogas", "Ethanol", "Biodiesel", "Electricity" ]
     if mode == "Car" and fuel in otherFuels:
         # translate to actual values
-        otherFuels[2] = "Mix natural/biogas"
-        otherFuels[5] = "Biodiesel 100%"
+        fuelIdx = otherFuels.index(fuel)
+        otherFuels = [ "gasoline", "gas (fossil)", "gas (bio)", "ethanol", "hvo 100", "electricity" ]
 
-        # pick fuel form(s)
-        els = browser.find_elements(By.XPATH, "//div[contains(@class, 'field--name-field-drivmedel')]/div/div")
-        # work around potential staleness issues when there are several forms
-        fuelEls = len(els)
-        # //*[starts-with(@name,'B')]
-        # import pdb; pdb.set_trace()
-        for fidx in range(fuelEls):
-            el = els[fidx]
-            if not el.is_displayed():
-                continue
-
-            # open fuel menu
-            fmenu = el.find_element(By.XPATH, "a[@class='nl-field-toggle']")
-            fmenu.click()
-            fakeWait(1)
-
-            # get correct li index
-            # NOTE: could not use .select() to simplify, since it's hidden
-            # try block just to make testing easier
-            try:
-                idx = otherFuels.index(fuel) + 1
-            except:
-                idx = 1
-            if idx > 1:
-                idx = idx + 3
-
-            # confirm fuel choice
-            fuel = el.find_element(By.XPATH, "ul/li[{}]".format(idx))
-            fuel.click()
-            #  wait until it recalibrates
-            fakeWait(2)
-            # refetch list, since the DOM changed
-            els = browser.find_elements(By.XPATH, "//div[contains(@class, 'field--name-field-drivmedel')]/div/div")
-
-    # close by reclicking on the main element (x to close isn't interactable)
-    browser.find_element(By.XPATH, "//div[starts-with(@id, 'edit-field-resvagar-wrapper')]").click()
+        # pick fuel
+        els = browser.find_element(By.XPATH, "//select[contains(@*,'selectedDrivingFuel')]")
+        select = Select(els)
+        select.select_by_visible_text(otherFuels[fuelIdx])
+        fakeWait(2)
 
     # choose random useless accommodation, so we can trigger the final calculation
-    browser.find_elements(By.XPATH, "//div[@class='accommodation-footer']/div[1]")[0].click()
-    waitForVisible(30, (By.XPATH, "//input[@name=\'calculate_total\']"))
-    browser.find_element(By.XPATH, "//input[@name=\'calculate_total\']").click()
-    waitForVisible(30, (By.CSS_SELECTOR, ".total-emissions"))
+    browser.find_element(By.CLASS_NAME, "t-accommodation").click()
+    resultH2 = '//h2[small[text()="kg"]]'
+    waitForVisible(5, (By.XPATH, resultH2))
 
     # grab and clean up the result
-    emissions = browser.find_element(By.CSS_SELECTOR, ".total-emissions").text
+    emissions = browser.find_element(By.XPATH, resultH2).text
     kg = round(int(emissions.split()[0]) * scaleFactor)
     if originalMode == "Bike" or originalMode == "Walk":
         km = 0
